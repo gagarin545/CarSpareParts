a2876bb06e0e782bae27d4e2ef668b2937190a04
gagarin-yf <ura545@mail.ru>
2020-02-10 15:28:43 +0500

    new task

--- src/main/java/ru/repository/PartsEntityRepository.java
+++ src/main/java/ru/repository/PartsEntityRepository.java
@@ -1,8 +1,16 @@
 package ru.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.entity.GroupPartsEntity;
+import ru.entity.ModelCarEntity;
 import ru.entity.PartsEntity;
+import ru.entity.SparePartsEntity;
+
+import java.util.List;
 
 public interface PartsEntityRepository extends JpaRepository<PartsEntity, Long> {
-    
+    @Query("select b from PartsEntity b where b.modelCarEntity = :model and b.groupPartsEntity = :group and b.sparePartsEntity = :spare")
+    List<PartsEntity> findByNumer(@Param("model") ModelCarEntity model, @Param("group")GroupPartsEntity group, @Param("spare")SparePartsEntity spare);
 }
---
+++ src/test/java/ru/PartsTest.java
@@ -0,0 +1,53 @@
+package ru;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.entity.GroupPartsEntity;
+import ru.entity.ModelCarEntity;
+import ru.entity.PartsEntity;
+import ru.entity.SparePartsEntity;
+import ru.service.GroupService;
+import ru.service.ModelService;
+import ru.service.PartServise;
+import ru.service.SpareService;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PartsTest {
+    @Resource
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    @Resource
+    PartServise partServise;
+
+    @Resource
+    SpareService spareService;
+
+    @Resource
+    ModelService modelService;
+    @Resource
+    GroupService groupService;
+
+    @Before
+    public void setUp() throws Exception {        em = emf.createEntityManager();    }
+
+    @Test
+    public void  test() {
+        SparePartsEntity spare = spareService.spare(  1L );
+        ModelCarEntity model = modelService.model(1L);
+        GroupPartsEntity group = groupService.group( 2L);
+        List<PartsEntity> parts = partServise.part(model, group, spare);
+        System.out.println(spare.getSpare_name());
+        parts.forEach(x-> System.out.println( x.getCost() + x.getPhoto()));
+
+    }
+}
